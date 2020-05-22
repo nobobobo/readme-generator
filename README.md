771ee65d3074cf263a2cc20bdc1e3b51180c34dd
nobobobo <ianhsu1221@gmail.com>
2020-05-22 16:46:18 -0700

    add fs module

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# readme-generator
-Node.js README.md generator
+# readme generator
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-inquirer.prompt({
-    type:"input",
-    message: "What is your GitHub repository name?",
-    name: "repoName"
-}).then(function(response){
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your GitHub repository name?",
+        name: "repoName"
+    },{
+        
+    }
+    ]).then(response => {
     console.log(response);
 })
+
+
+const createNewReadme = () => {
+    fs.write("README.md", "");
+}
+
+const addNewLine = (text, prefix) => {
+    newLine = `${prefix} ${text}`;
+    fs.appendFile("README.md", `${newLine}\n`);
+}
+
