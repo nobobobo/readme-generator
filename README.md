cd3903d766a34a13df5fb4869492428bee44c93a
nobobobo <ianhsu1221@gmail.com>
2020-05-22 17:13:41 -0700

    add feature to create table of contents to readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# readme generator
+#  Hello  
+
+ 1. [ Description ](#desc) 
+ <a name="desc"></a> 
+##  1. Description 
+
+ this is desc 
+
--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is your GitHub repository name?",
-        name: "repoName"
-    },{
-        
-    }
-    ]).then(response => {
-    console.log(response);
-})
-
 
 const createNewReadme = () => {
-    fs.write("README.md", "");
+    fs.writeFileSync("README.md", "", err => {
+        if (err) return console.log(err);
+    });
 }
 
-const addNewLine = (text, prefix) => {
-    newLine = `${prefix} ${text}`;
-    fs.appendFile("README.md", `${newLine}\n`);
+const addText = (text, prefix, suffix) => {
+    newText = `${prefix} ${text} ${suffix}`;
+    fs.appendFileSync("README.md", newText, err => {
+        if (err) return console.log(err);
+    });
 }
 
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your Project title?",
+            name: "title"
+        }, {
+            type: "input",
+            message: "What is your Project description?",
+            name: "desc"
+        }
+
+    ])
+    .then(
+        response => {
+            // create a new README.md in current directory
+            createNewReadme();
+
+            // append title
+            addText(response.title, "# ", "\n\n");
+
+            // append table of contents
+
+            addText("1. [ Description ](#desc)", "", "\n");
+
+            // append description
+            addText('<a name="desc"></a>', "", "\n");
+            addText('1. Description', "## ", "\n\n");
+            addText(response.desc, "", "\n\n");
+        })
