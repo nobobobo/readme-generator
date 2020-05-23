983771fcba9fbe29286038fc3c18b09d31032f41
nobobobo <ianhsu1221@gmail.com>
2020-05-22 18:13:17 -0700

    added new prompts

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-#  Hello  
+# README AutoGenerator
 
- 1. [ Description ](#desc) 
- <a name="desc"></a> 
-##  1. Description 
+1. [ Description ](#desc)
+2. [ Installation ](#install)
+3. [ Usage ](#usage)
+4. [ License ](#license)
+5. [ Contributing ](#contribute)
+6. [ Tests ](#test)
+7. [ Questions ](#question)
 
- this is desc 
+<a name="desc"></a>
+ ## 1. Description 
+
+This command line program generates a README.md file
+
+<a name="install"></a>
+ ## 2. Installation 
+
+```npm install readme-generator```
+
+<a name="usage"></a>
+ ## 3. Usage 
+
+```node index.js```
 
--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const createNewReadme = () => {
     });
 }
 
-const addText = (text, prefix, suffix) => {
-    newText = `${prefix} ${text} ${suffix}`;
-    fs.appendFileSync("README.md", newText, err => {
+const addText = text => {
+    fs.appendFileSync("README.md", text, err => {
         if (err) return console.log(err);
     });
 }
@@ -20,12 +19,20 @@ inquirer
     .prompt([
         {
             type: "input",
-            message: "What is your Project title?",
+            message: "What is your project title?",
             name: "title"
         }, {
             type: "input",
-            message: "What is your Project description?",
+            message: "What is your project description?",
             name: "desc"
+        }, {
+            type: "input",
+            message: "How to install your project?",
+            name: "install"
+        }, {
+            type: "input",
+            message: "How to use your project?",
+            name: "usage"
         }
 
     ])
@@ -35,14 +42,26 @@ inquirer
             createNewReadme();
 
             // append title
-            addText(response.title, "# ", "\n\n");
+            title = `# ${response.title}\n\n`;
+            addText(title);
 
             // append table of contents
 
-            addText("1. [ Description ](#desc)", "", "\n");
+            addText("1. [ Description ](#desc)\n2. [ Installation ](#install)\n3. [ Usage ](#usage)\n4. [ License ](#license)\n5. [ Contributing ](#contribute)\n6. [ Tests ](#test)\n7. [ Questions ](#question)\n\n");
 
             // append description
-            addText('<a name="desc"></a>', "", "\n");
-            addText('1. Description', "## ", "\n\n");
-            addText(response.desc, "", "\n\n");
+            description = `<a name="desc"></a>\n ## 1. Description \n\n${response.desc}\n\n`;
+            addText(description);
+
+
+            // append installtion
+            installation = `<a name="install"></a>\n ## 2. Installation \n\n\`\`\`${response.install}\`\`\`\n\n`;
+            addText(installation);
+
+
+            // append Usage
+            usage = `<a name="usage"></a>\n ## 3. Usage \n\n\`\`\`${response.usage}\`\`\`\n\n`;
+            addText(usage);
+
+
         })
