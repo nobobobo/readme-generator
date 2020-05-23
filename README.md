9b54a32b56bc4510314ba95dc75ac724252c4712
nobobobo <ianhsu1221@gmail.com>
2020-05-22 20:01:40 -0700

    added some inquirer questions and badges

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# README AutoGenerator
+# Readme Auto Generator 
 
+[![Generic badge](https://img.shields.io/badge/VERSION-0.0.1-GREEN.svg)](https://shields.io/)
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
 1. [ Description ](#desc)
 2. [ Installation ](#install)
 3. [ Usage ](#usage)
@@ -11,15 +13,19 @@
 <a name="desc"></a>
  ## 1. Description 
 
-This command line program generates a README.md file
+This command line program dynamically generates a README.md file based on the user's input.
 
 <a name="install"></a>
  ## 2. Installation 
 
-```npm install readme-generator```
+```
+npm install
+```
 
 <a name="usage"></a>
  ## 3. Usage 
 
-```node index.js```
+```
+node index.js
+```
 
--- index.js
+++ index.js
@@ -19,22 +19,34 @@ inquirer
     .prompt([
         {
             type: "input",
-            message: "What is your project title?",
+            message: "Project title: ",
             name: "title"
         }, {
             type: "input",
-            message: "What is your project description?",
+            message: "Project version: ",
+            name: "version"
+        }, {
+            type: "input",
+            message: "Project description: ",
             name: "desc"
         }, {
             type: "input",
-            message: "How to install your project?",
+            message: "Install command: ",
             name: "install"
         }, {
             type: "input",
-            message: "How to use your project?",
+            message: "Usage command: ",
             name: "usage"
+        }, {
+            type: "list",
+            message: "License name: ",
+            choices: ['MIT License', 'GNU General Public License (GPL)', 'Creative Commons Licenses', 'Other'],
+            name: "license"
+        }, {
+            type: "input",
+            message: "Author name: ",
+            name: "author"
         }
-
     ])
     .then(
         response => {
@@ -44,6 +56,26 @@ inquirer
             // append title
             title = `# ${response.title}\n\n`;
             addText(title);
+
+            // append a badge for version
+            // [![Generic badge](https://img.shields.io/badge/<SUBJECT>-<STATUS>-<COLOR>.svg)](https://shields.io/)
+
+            version = `[![Generic badge](https://img.shields.io/badge/VERSION-${response.version}-GREEN.svg)](https://shields.io/)\n`;
+            addText(version);
+
+            // append a badge for license
+            if (response.license === "MIT License"){
+                badge = '[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)\n'
+            } else if (response.license === "GNU General Public License (GPL)"){
+                badge = '[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)\n'
+            } else if (response.license === "Creative Commons Licenses"){
+                badge = '[![CC-0 license](https://img.shields.io/badge/License-CC--0-blue.svg)](https://creativecommons.org/licenses/by-nd/4.0)\n'
+            } else {
+                badge = '\n';
+            }
+
+            addText(badge);
+
 
             // append table of contents
 
@@ -55,13 +87,15 @@ inquirer
 
 
             // append installtion
-            installation = `<a name="install"></a>\n ## 2. Installation \n\n\`\`\`${response.install}\`\`\`\n\n`;
+            installation = `<a name="install"></a>\n ## 2. Installation \n\n\`\`\`\n${response.install}\n\`\`\`\n\n`;
             addText(installation);
 
 
             // append Usage
-            usage = `<a name="usage"></a>\n ## 3. Usage \n\n\`\`\`${response.usage}\`\`\`\n\n`;
+            usage = `<a name="usage"></a>\n ## 3. Usage \n\n\`\`\`\n${response.usage}\n\`\`\`\n\n`;
             addText(usage);
 
 
         })
+
+
